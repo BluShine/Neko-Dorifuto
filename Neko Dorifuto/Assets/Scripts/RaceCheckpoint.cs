48d5624e092f238f76ae196251b5bf5c6a6426b4
Mark T <BluShine@users.noreply.github.com>
2018-04-22 07:10:34 -0700

    Race checkpoints

---
+++ Neko Dorifuto/Assets/Scripts/RaceCheckpoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceCheckpoint : MonoBehaviour {
+
+    public GameObject checkpointGraphics;
+
+    RaceManager manager;
+    bool activated = false;
+
+	// Use this for initialization
+	void Awake () {
+        manager = FindObjectOfType<RaceManager>();
+        checkpointGraphics.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Activate()
+    {
+        activated = true;
+        checkpointGraphics.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(activated && other.gameObject.layer == 9)
+        {
+            activated = false;
+            checkpointGraphics.SetActive(false);
+            manager.HitCheckpoint();
+        }
+    }
+}
---
+++ Neko Dorifuto/Assets/Scripts/RaceManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceManager : MonoBehaviour {
+
+    public float targetTime = 60;
+
+    public List<RaceCheckpoint> checkpoints;
+    int currentCheckpoint = 0;
+
+    public float currentLapTime = 0;
+    public int laps = 0;
+
+    public void HitCheckpoint()
+    {
+        currentCheckpoint++;
+        if(currentCheckpoint == checkpoints.Count)
+        {
+            currentLapTime = 0;
+            currentCheckpoint = 0;
+            laps++;
+        }
+        checkpoints[currentCheckpoint].Activate();
+    }
+
+	// Use this for initialization
+	void Start () {
+        checkpoints[0].Activate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        currentLapTime += Time.fixedDeltaTime;
+    }
+}
