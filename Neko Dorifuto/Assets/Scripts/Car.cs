222f05caf04cb977f37cd99bcc92c120e9963a5e
Mark T <BluShine@users.noreply.github.com>
2018-04-22 01:43:18 -0700

    Driving physics and track refining

---
+++ Neko Dorifuto/Assets/Scripts/Car.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    Rigidbody body;
+
+    public float gravity = 10;
+    public float topSpeed = 50;
+    public float drag = 1;
+    public AnimationCurve accelCurve;
+    public float accelForce = 20;
+    public float energyLoss = 5;
+    public float brakeForce = 40;
+    public float tractionForce = 20;
+    public float steeringSpeed = 10;
+    public float steeringAccel = 10;
+    public AnimationCurve steeringTopSpeedCurve;
+    public float steeringTilt = 20;
+    public float targetSuspension = .5f;
+    public float suspensionForce = 20;
+    public float suspensionUprightForce = 1;
+    public float airRotationDrag = 1;
+
+    HoverPIDController hoverPID;
+    public HoverPIDProperties hoverPIDProperties;
+
+    public LayerMask raycastMask;
+    static float RAYRADIUS = .3f;
+    static float RAYLENGTH = 1;
+
+    private bool isColliding = false;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+        hoverPID = new HoverPIDController(hoverPIDProperties);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isColliding = true;
+    }
+
+    private void FixedUpdate()
+    {
+        bool onGround = false;
+        Vector3 relativeVel = transform.InverseTransformDirection(body.velocity);
+        //suspension
+        #region suspension forces
+        //gravity
+        body.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+        //hover force
+        Vector3 upwardsDirection = transform.up;
+        RaycastHit rayhit = new RaycastHit();
+        Vector3 rayorigin = transform.position + transform.up * (RAYRADIUS + .1f);
+        Ray ray = new Ray(rayorigin, -transform.up);
+        if (Physics.SphereCast(ray, RAYRADIUS, out rayhit, RAYLENGTH, raycastMask))
+        {
+            float compression = Vector3.Distance(rayorigin, rayhit.point);
+            float error = targetSuspension - compression;
+            float upwardsForce = hoverPID.updatePosition(error, Time.fixedDeltaTime) * suspensionForce;
+            body.AddForce(transform.up * upwardsForce, ForceMode.Acceleration);
+            upwardsDirection = rayhit.normal;
+            onGround = true;
+        }
+        //keep upright forces
+        if (isColliding && transform.up.y < 0)
+        {
+            upwardsDirection = Vector3.up;
+            body.AddTorque(hoverPID.updateRotation(transform, upwardsDirection, Time.fixedDeltaTime) * 50, ForceMode.Acceleration);
+
+        }
+        else if(onGround)
+        {
+            body.AddTorque(hoverPID.updateRotation(transform, upwardsDirection, Time.fixedDeltaTime) * suspensionUprightForce, ForceMode.Acceleration);
+        } else
+        {
+            body.AddTorque(-body.angularVelocity.normalized * airRotationDrag, ForceMode.Acceleration);
+        }
+        #endregion
+        //acceleration
+        #region acceleration forces
+        //drag when surpassing top speed in forward or reverse
+        if(relativeVel.z > -topSpeed)
+        {
+            float vDrag = (relativeVel.z + topSpeed) * drag;
+            body.AddForce(-transform.forward * drag, ForceMode.Acceleration);
+        } 
+        else if (relativeVel.z > topSpeed)
+        {
+            float vDrag = (relativeVel.z - topSpeed) * drag;
+            body.AddForce(-transform.forward * drag, ForceMode.Acceleration);
+        }
+        //acceleration
+        if (onGround)
+        {
+            float accelOutput = accelForce * accelCurve.Evaluate(Mathf.Abs(relativeVel.z) / topSpeed);
+            if (Input.GetButton("Jump") && relativeVel.z < topSpeed)
+            {
+                //if we are close to top speed, set velocity to top speed.
+                if (accelOutput * Time.fixedDeltaTime + relativeVel.z > topSpeed)
+                {
+                    body.AddForce(transform.forward * (relativeVel.z - topSpeed), ForceMode.VelocityChange);
+                }
+                else
+                {
+                    body.AddForce(transform.forward * accelOutput, ForceMode.Acceleration);
+                }
+            }
+            else if (Input.GetButton("Fire1") && relativeVel.z > -topSpeed * .5f)
+            {
+                //if we are close to top speed, set velocity to top speed.
+                if (-accelOutput * Time.fixedDeltaTime + relativeVel.z < -topSpeed)
+                {
+                    body.AddForce(-transform.forward * (relativeVel.z + topSpeed * .5f), ForceMode.VelocityChange);
+                }
+                else
+                {
+                    body.AddForce(-transform.forward * accelOutput * .5f, ForceMode.Acceleration);
+                }
+            }
+        }
+        #endregion
+        //steering
+        #region steering forces
+        if (onGround) {
+            float turnVel = transform.InverseTransformDirection(body.angularVelocity).y;
+            if (Input.GetAxis("Horizontal") == 0)
+            {
+                if (Mathf.Abs(turnVel) < Time.fixedDeltaTime * steeringAccel)
+                {
+                    body.AddRelativeTorque(new Vector3(0, -turnVel, 0), ForceMode.VelocityChange);
+                } else
+                {
+                    body.AddRelativeTorque(new Vector3(0, -Mathf.Sign(turnVel), 0) * steeringAccel, ForceMode.Acceleration);
+                }
+            } else
+            {
+                if(Mathf.Abs(turnVel) > steeringSpeed)
+                {
+                    body.AddTorque(new Vector3(0, -Mathf.Sign(turnVel), 0) * steeringAccel, ForceMode.Acceleration);
+                }
+                else if(Input.GetAxis("Horizontal") > 0 && 
+                    turnVel + steeringAccel * Time.fixedDeltaTime > steeringSpeed)
+                {
+                    body.AddRelativeTorque(new Vector3(0, steeringSpeed - turnVel, 0), ForceMode.VelocityChange);
+                }
+                else if (Input.GetAxis("Horizontal") < 0 &&
+                  turnVel - steeringAccel * Time.fixedDeltaTime < -steeringSpeed)
+                {
+                    body.AddRelativeTorque(new Vector3(0, -steeringSpeed - turnVel, 0), ForceMode.VelocityChange);
+                }
+                else
+                {
+                    body.AddRelativeTorque(new Vector3(0, Input.GetAxis("Horizontal") * steeringAccel, 0), ForceMode.Acceleration);
+                }
+            }
+        }
+        #endregion
+        //traction force
+        #region traction forces
+        if (onGround)
+        {
+            float horizontalVel = relativeVel.x;
+            Vector2 skiddingNormal = new Vector2(relativeVel.x, relativeVel.z).normalized;
+            if (Mathf.Abs(horizontalVel) < Time.deltaTime * tractionForce)
+            {
+                body.AddRelativeForce(-horizontalVel * Vector3.right, ForceMode.VelocityChange);
+            }
+            else
+            {
+                body.AddRelativeForce(Mathf.Sign(-horizontalVel) * Vector3.right * tractionForce, ForceMode.Acceleration);
+            }
+            //forwards friction
+            float forwardVelocity = relativeVel.z;
+            float skidAmount = Mathf.Min(Mathf.Abs(skiddingNormal.x), Mathf.Abs(skiddingNormal.y)) / .707107f; //how close we are to a 45-degree skid
+            if (energyLoss * Time.fixedDeltaTime > Mathf.Abs(forwardVelocity))
+            {
+                body.AddRelativeForce(Vector3.forward * -forwardVelocity, ForceMode.VelocityChange);
+            }
+            else
+            {
+                body.AddRelativeForce(Vector3.forward * energyLoss * -Mathf.Sign(forwardVelocity), ForceMode.Acceleration);
+            }
+            //braking
+            if (!Input.GetButton("Jump") && !Input.GetButton("Fire1"))
+            {
+                Vector2 groundVel = new Vector2(relativeVel.x, relativeVel.z);
+               if(groundVel.magnitude < brakeForce * Time.fixedDeltaTime)
+                {
+                    body.AddRelativeForce(new Vector3(-relativeVel.x, 0, -relativeVel.z), ForceMode.VelocityChange);
+                } else
+                {
+                    groundVel.Normalize();
+                    body.AddRelativeForce(new Vector3(groundVel.x, 0, groundVel.y) * -brakeForce, ForceMode.Acceleration);
+                }
+            }
+        }
+        #endregion
+
+        isColliding = false;
+    }
+}
---
+++ Neko Dorifuto/Assets/Scripts/HoverPIDProperties.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HoverPIDProperties", menuName = "BluShine/HoverPID", order = 1)]
+public class HoverPIDProperties : ScriptableObject {
+
+    public float hoverP = 1;
+    public float hoverI = .1f;
+    public float hoverD = .1f;
+
+    public float rotationP = 1;
+    public float rotationI = .1f;
+    public float rotationD = .1f;
+	
+}
+
+public class HoverPIDController
+{
+    public HoverPIDProperties properties;
+    float posIntegrator = 0;
+    float posLastError = 0;
+    Vector2 rotIntegrator = Vector2.zero;
+    Vector2 rotLastError = Vector2.zero;
+
+    public HoverPIDController(HoverPIDProperties prop)
+    {
+        properties = prop;
+    }
+
+    //returns force
+    public float updatePosition(float error, float deltaTime, float minForce = 0, float maxForce = 1)
+    {
+        //i
+        posIntegrator += error * deltaTime;
+        posIntegrator = Mathf.Clamp(posIntegrator, minForce, maxForce);
+        //d
+        float deriv = (error - posLastError) / deltaTime;
+        posLastError = error;
+        //force
+        float force = error * properties.hoverP + posIntegrator * properties.hoverI + deriv * properties.hoverD;
+        force = Mathf.Clamp(force, minForce, maxForce);
+        return force;
+    }
+
+    //returns torque force vector in local space
+    public Vector3 updateRotation(Transform transf, Vector3 upDir, float deltaTime)
+    {
+        //calculate pitch and roll error in local space
+        Vector3 error = upDir - transf.up;
+        Vector3 errorLocal = transf.InverseTransformDirection(error);
+        Vector2 errorXZ = new Vector2(errorLocal.z, -errorLocal.x);//axes seem weird, don't worry about it :P
+        //i
+        rotIntegrator += errorXZ;
+        rotIntegrator = new Vector2(Mathf.Clamp(rotIntegrator.x, -1, 1), Mathf.Clamp(rotIntegrator.y, -1, 1));
+        //d
+        Vector2 deriv = (errorXZ - rotLastError) / deltaTime;
+        rotLastError = errorXZ + Vector2.zero;
+        //force
+        Vector2 forceXZ = errorXZ * properties.rotationP + rotIntegrator * properties.rotationI + deriv * properties.rotationD;
+        forceXZ = new Vector2(Mathf.Clamp(forceXZ.x, -1, 1), Mathf.Clamp(forceXZ.y, -1, 1));
+        Vector3 worldForce = transf.TransformDirection(new Vector3(forceXZ.x, 0, forceXZ.y));
+        return worldForce;
+        /*
+        //p
+        float error = Vector3.Angle(transf.TransformDirection(upDir), Vector3.up);//do this in local space for better behavior
+        //i
+        rotIntegrator += error * deltaTime;
+        rotIntegrator = Mathf.Clamp(rotIntegrator, -maxForce, maxForce);
+        //d
+        float deriv = (error - rotLastError) / deltaTime;
+        rotLastError = error;
+        //force
+        float force = error * properties.hoverP + rotIntegrator * properties.hoverI + deriv * properties.hoverD;
+        force = Mathf.Clamp(force, -maxForce, maxForce);
+        //cross product is the axis of rotation from our current vector to our target vector
+        Vector3 cross = Vector3.Cross(transf.TransformDirection(upDir), Vector3.up);
+        return cross * force;*/
+    }
+}
